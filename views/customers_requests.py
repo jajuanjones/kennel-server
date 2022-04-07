85175b195a5a858fc60d54737ffae8f9fd761238
Jajuan Jones <jajuanojones2@gmail.com>
2022-04-07 14:25:46 -0500

    added a create customer method in POST function

--- views/customers_requests.py
+++ views/customers_requests.py
@@ -24,3 +24,17 @@ def get_single_customer(id):
             request_customer = customer
 
     return request_customer
+
+def create_customer(customer):
+    """This function will create a new customer
+    """
+    # get the max id from the end of the customers list
+    max_id = CUSTOMERS[-1]["id"]
+    # set new id to the max id plus 1
+    new_id = max_id + 1
+    # set our passing customer param id as the new id
+    customer["id"] = new_id
+    # append our new customer to the end of the customers list
+    CUSTOMERS.append(customer)
+    # return our customer param
+    return customer
