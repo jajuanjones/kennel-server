f4ecf1ff4304a1fe11c4595f4ba439723036548b
Jajuan Jones <jajuanojones2@gmail.com>
2022-04-08 11:17:39 -0500

    added a function to update a location with new data

--- views/__init__.py
+++ views/__init__.py
@@ -1,7 +1,7 @@
 from .animal_requests import get_all_animals, get_single_animal
 from .animal_requests import create_animal, delete_animal, update_animal
 from .locations_requests import get_all_locations, get_single_location
-from .locations_requests import create_location, delete_location
+from .locations_requests import create_location, delete_location, update_location
 from .employees_requests import get_all_employees, get_single_employee
 from .employees_requests import create_employee, delete_employee
 from .customers_requests import get_all_customers, get_single_customer
