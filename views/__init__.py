2171f073f6ec02319d40674414f83151de17f23c
Jajuan Jones <jajuanojones2@gmail.com>
2022-04-08 11:06:42 -0500

    added an update fuction in customers module for PUT requests on customer list

--- views/__init__.py
+++ views/__init__.py
@@ -5,4 +5,4 @@ from .locations_requests import create_location, delete_location
 from .employees_requests import get_all_employees, get_single_employee
 from .employees_requests import create_employee, delete_employee
 from .customers_requests import get_all_customers, get_single_customer
-from .customers_requests import create_customer, delete_customer
+from .customers_requests import create_customer, delete_customer, update_customer
